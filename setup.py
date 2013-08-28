58b7c7bb8dd1f60a923e5781527be1396c13431a
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2013-08-28 00:06:45 -0700

    added changes suggested in plone book

--- setup.py
+++ setup.py
@@ -15,9 +15,9 @@ setup(name='declinefm.policy',
         "Programming Language :: Python",
         ],
       keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
+      author='Manuel Amador (Rudd-O)',
+      author_email='rudd-o@rudd-o.com',
+      url='http://declinefm.com/',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['declinefm'],
@@ -25,14 +25,15 @@ setup(name='declinefm.policy',
       zip_safe=False,
       install_requires=[
           'setuptools',
-          # -*- Extra requirements: -*-
+          'Plone',
       ],
+      extras_require={
+          'test': ['plone.app.testing'],
+      },
       entry_points="""
       # -*- Entry points: -*-
 
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      setup_requires=["PasteScript"],
-      paster_plugins=["ZopeSkel"],
       )
