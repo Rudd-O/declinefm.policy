22527787f541a298e304b6352ddd5661dd7e7c66
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2013-10-06 00:19:23 -0700

    portlet tests added

---
+++ declinefm/policy/tests/test_latestnews.py
@@ -0,0 +1,94 @@
+from zope.component import getUtility, getMultiAdapter
+
+from plone.portlets.interfaces import IPortletType
+from plone.portlets.interfaces import IPortletManager
+from plone.portlets.interfaces import IPortletAssignment
+from plone.portlets.interfaces import IPortletDataProvider
+from plone.portlets.interfaces import IPortletRenderer
+
+from plone.app.portlets.storage import PortletAssignmentMapping
+
+from declinefm.policy.portlets import latestnews
+from declinefm.policy.portlets.tests.base_latestnews import TestCase
+
+
+class TestPortlet(TestCase):
+
+    def afterSetUp(self):
+        self.setRoles(('Manager',))
+
+    def test_portlet_type_registered(self):
+        portlet = getUtility(IPortletType, name='declinefm.policy.portlets.LatestNews')
+        self.assertEquals(portlet.addview, 'declinefm.policy.portlets.LatestNews')
+
+    def test_interfaces(self):
+        # TODO: Pass any keywoard arguments to the Assignment constructor
+        portlet = latestnews.Assignment()
+        self.failUnless(IPortletAssignment.providedBy(portlet))
+        self.failUnless(IPortletDataProvider.providedBy(portlet.data))
+
+    def test_invoke_add_view(self):
+        portlet = getUtility(IPortletType, name='declinefm.policy.portlets.LatestNews')
+        mapping = self.portal.restrictedTraverse('++contextportlets++plone.leftcolumn')
+        for m in mapping.keys():
+            del mapping[m]
+        addview = mapping.restrictedTraverse('+/' + portlet.addview)
+
+        # TODO: Pass a dictionary containing dummy form inputs from the add form
+        addview.createAndAdd(data={})
+
+        self.assertEquals(len(mapping), 1)
+        self.failUnless(isinstance(mapping.values()[0], latestnews.Assignment))
+
+    # NOTE: This test can be removed if the portlet has no edit form
+    def test_invoke_edit_view(self):
+        mapping = PortletAssignmentMapping()
+        request = self.folder.REQUEST
+
+        mapping['foo'] = latestnews.Assignment()
+        editview = getMultiAdapter((mapping['foo'], request), name='edit')
+        self.failUnless(isinstance(editview, latestnews.EditForm))
+
+    def test_obtain_renderer(self):
+        context = self.folder
+        request = self.folder.REQUEST
+        view = self.folder.restrictedTraverse('@@plone')
+        manager = getUtility(IPortletManager, name='plone.rightcolumn', context=self.portal)
+
+        # TODO: Pass any keywoard arguments to the Assignment constructor
+        assignment = latestnews.Assignment()
+
+        renderer = getMultiAdapter((context, request, view, manager, assignment), IPortletRenderer)
+        self.failUnless(isinstance(renderer, latestnews.Renderer))
+
+
+class TestRenderer(TestCase):
+
+    def afterSetUp(self):
+        self.setRoles(('Manager',))
+
+    def renderer(self, context=None, request=None, view=None, manager=None, assignment=None):
+        context = context or self.folder
+        request = request or self.folder.REQUEST
+        view = view or self.folder.restrictedTraverse('@@plone')
+        manager = manager or getUtility(IPortletManager, name='plone.rightcolumn', context=self.portal)
+
+        # TODO: Pass any default keywoard arguments to the Assignment constructor
+        assignment = assignment or latestnews.Assignment()
+        return getMultiAdapter((context, request, view, manager, assignment), IPortletRenderer)
+
+    def test_render(self):
+        # TODO: Pass any keywoard arguments to the Assignment constructor
+        r = self.renderer(context=self.portal, assignment=latestnews.Assignment())
+        r = r.__of__(self.folder)
+        r.update()
+        output = r.render()
+        # TODO: Test output
+
+
+def test_suite():
+    from unittest import TestSuite, makeSuite
+    suite = TestSuite()
+    suite.addTest(makeSuite(TestPortlet))
+    suite.addTest(makeSuite(TestRenderer))
+    return suite
